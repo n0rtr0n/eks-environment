c8ae9462d4d50fdbdd7fc24db15aec4642c555be
Nathan Norton <notnortron@gmail.com>
2024-08-15 17:44:15 -0700

    Remove unused state

--- eks-cluster/dev/variables.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
-  env             = "dev"
-  vpc_cidr        = "10.0.0.0/16"
-  vpc_name        = "eks-testing"
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-  region          = "us-west-2"
-}
